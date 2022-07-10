19cb0104ab51a62017ec4ef8ffa9ca6f248e6b93
ibrahim hamzat <hamat.ibrahim@gmail.com>
2022-07-10 15:31:20 +0100

    initial commit

---
+++ reservation/models.py
@@ -0,0 +1,23 @@
+from pyexpat import model
+from django.db import models
+
+
+
+class Rental(models.Model):
+    name = models.CharField(max_length = 255)
+
+    def __str___(self):
+            return self.name
+
+
+
+class Reservation(models.Model):
+    check_in = models.DateField()
+    check_out = models.DateField()
+    rental = models.ForeignKey(Rental,on_delete=models.CASCADE)
+    previous_reservation_id = models.IntegerField()
+    
+
+
+
+
